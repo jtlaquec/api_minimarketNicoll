f9e010d8fc32d7210f3516a83dedf4d98db15f54
jtlaquec <78872016+jtlaquec@users.noreply.github.com>
2023-07-12 18:42:27 -0500

    Update CategoriaController.php

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -25,19 +25,10 @@ class CategoriaController extends Controller
             return response()->json(['error' => $validator->errors()], 400);
         }
     
-        $nombre = $request->input('nombre',null);
-        $descripcion = $request->input('descripcion',null);
-    
-        $categoria = Categoria::firstOrNew(['nombre' => $nombre]);
-    
-        if ($categoria->exists && $categoria->estado == 0) {
-            $categoria->estado = 1;
-            $categoria->descripcion = $descripcion;
-            $categoria->save();
-        } elseif (!$categoria->exists) {
-            $categoria->descripcion = $descripcion;
-            $categoria->save();
-        }
+        $categoria = new Categoria();
+        $categoria->nombre = $request->input('nombre', null);
+        $categoria->descripcion = $request->input('descripcion', null);
+        $categoria->save();
     
         return response()->json(['message' => 'La categoría ha sido agregada correctamente'],200);
     }
@@ -60,23 +51,11 @@ class CategoriaController extends Controller
             return response()->json(['error' => $validator->errors()], 400);
         }   
         
-        $nombre = $request->filled('nombre') ? $request->input('nombre') : $categoria->nombre;
-        $descripcion = $request->filled('descripcion') ? $request->input('descripcion') : $categoria->descripcion;
-        
-        $categoria1 = Categoria::firstOrNew(['nombre' => $nombre]);
+        $categoria->nombre = $request->filled('nombre') ? $request->input('nombre') : $categoria->nombre;
+        $categoria->descripcion = $request->filled('descripcion') ? $request->input('descripcion') : $categoria->descripcion;
+        $categoria->save();
 
-        if ($categoria->exists && $categoria->estado == 0) {
-            $categoria->estado = 0;
-            $categoria1->estado = 1;
-            $categoria->descripcion = $descripcion;
-            $categoria->save();
-            $categoria1->save();
-        } elseif (!$categoria->exists) {
-            $categoria->nombre = $nombre;
-            $categoria->descripcion = $descripcion;
-            $categoria->save();
-        }
-    
+
         return response()->json(['message' => 'La categoría ha sido modificada correctamente'],200);
     }
 
